b3c4deaaf56b7e10c015db9751618cd19731dd2d
irina-marzioni-tdf <104698059+irina-marzioni-tdf@users.noreply.github.com>
2022-06-02 23:05:10 -0300

    initial commit

--- EJERCICIOS-FILMINA-5-ARREGLOS/SUMAR-DOS-ARREGLOS/sumarDosArreglos.ts
+++ EJERCICIOS-FILMINA-5-ARREGLOS/SUMAR-DOS-ARREGLOS/sumarDosArreglos.ts
@@ -1,30 +1,36 @@
-let vector1: number[] = new Array (6);
-let vector2: number[] = new Array (6);
-let vectorResultante : number[] = new Array (6);
+let vector1: number[] = new Array(6);
+let vector2: number[] = new Array(6);
+let vectorResultante: number[] = new Array(6);
 
-function cargarVector (vector:number[]) {
-    for (let i:number = 0; i < 6; i++) {
-        vector[i] = Number(prompt("Ingrese el valor " + (i+1) + " : "));
-    }
+function cargarVector() {
+  let vector: number[] = new Array(6);
+  for (let i: number = 0; i < 6; i++) {
+    vector[i] = Number(prompt("Ingrese el valor " + (i + 1) + " : "));
+  }
+  return vector;
 }
 
-function sumarVectores (vector1, vector2, vectorResultante:number[]) {
-    for (let i:number = 0; i < 6; i++ ) {
-        vectorResultante[i] = vector1[i] + vector2[i];
-    }
+function sumarVectores(vector1, vector2) {
+  let resultado: number[] = new Array(6);
+  for (let i: number = 0; i < 6; i++) {
+    resultado[i] = vector1[i] + vector2[i];
+  }
+  return resultado;
 }
 
-alert("Carga de números para el vector 1")
-cargarVector(vector1);
-alert("Carga de números para el vector 2")
-cargarVector(vector2);
+alert("Carga de números para el vector 1");
+vector1 = cargarVector();
+alert("Carga de números para el vector 2");
+vector2 = cargarVector();
 
-sumarVectores(vector1,vector2,vectorResultante);
+vectorResultante = sumarVectores(vector1, vector2);
 
-alert("El resultado de la suma entre vector 1 y vector 2 se mostrará por consola")
-console.log ("vector 1 + vector 2 = vector resultante");
-for (let i:number = 0; i < 6; i++ ) {
-    console.log(vector1[i] + " + " + vector2[i] + " = " + vectorResultante[i]);
+alert(
+  "El resultado de la suma entre vector 1 y vector 2 se mostrará por consola"
+);
+console.log("vector 1 + vector 2 = vector resultante");
+for (let i: number = 0; i < 6; i++) {
+  console.log(vector1[i] + " + " + vector2[i] + " = " + vectorResultante[i]);
 }
 
 console.log(`Vector Resultante: [${vectorResultante}]`);
