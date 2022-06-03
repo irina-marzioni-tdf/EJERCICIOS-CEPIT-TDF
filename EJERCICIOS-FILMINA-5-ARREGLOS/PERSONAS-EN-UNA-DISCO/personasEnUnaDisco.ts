1f8eae30f30dc34242cf886e01f54c02e0e338e7
irina-marzioni-tdf <104698059+irina-marzioni-tdf@users.noreply.github.com>
2022-06-02 23:19:29 -0300

    initial commit

--- EJERCICIOS-FILMINA-5-ARREGLOS/PERSONAS-EN-UNA-DISCO/personasEnUnaDisco.ts
+++ EJERCICIOS-FILMINA-5-ARREGLOS/PERSONAS-EN-UNA-DISCO/personasEnUnaDisco.ts
@@ -1,6 +1,6 @@
-// Ejercicio entregable para el lunes 30/05 
+// Ejercicio entregable para el lunes 30/05
 // Nombre del Ejercicio: PERSONAS EN UNA DISCO
-let listaEdades:number[] = new Array(270);   
+let listaEdades: number[] = new Array(270);
 
 /*
 function imprimirEdades(listaEdades:number[]){
@@ -11,41 +11,42 @@ function imprimirEdades(listaEdades:number[]){
 */
 
 //-----------------------------------------------------------------------
-function mostrarInformacionDeEdades(listaEdades:number[]) {
-    let personasMenorA21:number = 0;
-    let personasMayorIgual21:number = 0;
+function mostrarInformacionDeEdades() {
+  let personasMenorA21: number = 0;
+  let personasMayorIgual21: number = 0;
 
-    for (let i :number = 0; i < listaEdades.length; i++) {
-        if (listaEdades[i] < 21){
-            personasMenorA21++;
-        }
-        else {
-            personasMayorIgual21++;
-        }
+  for (let i: number = 0; i < listaEdades.length; i++) {
+    if (listaEdades[i] < 21) {
+      personasMenorA21++;
+    } else {
+      personasMayorIgual21++;
     }
-    console.log(`Cantidad de personas menores a 21 años: ${personasMenorA21}`);
-    console.log(`Cantidad de personas mayores o iguales a 21 años: ${personasMayorIgual21}`);
-    console.log(`Total de personas dentro del local: ${listaEdades.length}`);
+  }
+  console.log(`Cantidad de personas menores a 21 años: ${personasMenorA21}`);
+  console.log(
+    `Cantidad de personas mayores o iguales a 21 años: ${personasMayorIgual21}`
+  );
+  console.log(`Total de personas dentro del local: ${listaEdades.length}`);
 }
 
 //------------------------------------------------------------------------
-function aleatorio(menorValor:number, mayorValor:number): number {
-    let edad:number = 0;
+function aleatorio(menorValor: number, mayorValor: number): number {
+  let edad: number = 0;
 
-    while ((edad < menorValor) || (edad > mayorValor)) {     
-        edad = Math.floor(Math.random() *100);          // obtendrá edades hasta que edad sea un valor que esté entre 18 y 40
-    }
-    return edad;                                        // 18 <= edad <= 40
+  while (edad < menorValor || edad > mayorValor) {
+    edad = Math.floor(Math.random() * 100); // obtendrá edades hasta que edad sea un valor que esté entre 18 y 40
+  }
+  return edad; // 18 <= edad <= 40
 }
 
-function cargarEdades (listaEdades:number[]) {
-    for (let i :number = 0; i < listaEdades.length; i++){
-        listaEdades[i] = aleatorio(18,40);
-    }
+function cargarEdades() {
+  for (let i: number = 0; i < listaEdades.length; i++) {
+    listaEdades[i] = aleatorio(18, 40);
+  }
 }
 //-------------------------------------------------------------------------
 
-cargarEdades(listaEdades);
-mostrarInformacionDeEdades(listaEdades);
+cargarEdades();
+mostrarInformacionDeEdades();
 
 //imprimirEdades(listaEdades);
