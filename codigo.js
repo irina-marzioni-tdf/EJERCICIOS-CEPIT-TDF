711940e8aa269f0a4b04a03fa7e9a20d8fd94f24
Valeria Sanabria <valeriasanabria@tdf.edu.ar>
2022-08-17 11:09:09 -0300

    first commit

---
+++ codigo.js
@@ -0,0 +1,26 @@
+let sumaTotal=0;
+let botonAgregar=document.querySelector(".btnAgregar");
+let botonTotal=document.querySelector(".btnTotal");
+
+botonAgregar.addEventListener("click",agregarProducto);
+botonTotal.addEventListener("click",calcularTotal);
+
+function agregarProducto() {
+    let producto= document.querySelector(".product").value;
+    let cantidad= document.querySelector(".cant").value;
+    let costo= document.querySelector(".cost").value;
+    let subTotal=(costo*cantidad);
+    let elementProd=document.createElement("li"); elementProd.innerHTML= producto;
+    let elementCant=document.createElement("li"); elementCant.innerHTML=cantidad;
+    let elementCost=document.createElement("li"); elementCost.innerHTML=`$` +costo;
+    let elementSubTotal=document.createElement("li"); elementSubTotal.innerHTML=`$` +subTotal;
+    listProd.appendChild(elementProd);
+    listCant.appendChild(elementCant);
+    listCost.appendChild(elementCost);
+    listSubTotal.appendChild(elementSubTotal);
+    sumaTotal= sumaTotal+subTotal;
+    }
+
+function calcularTotal() {
+    document.querySelector(".total").innerHTML=`El costo total de la compra es: $ `+ sumaTotal;
+}
